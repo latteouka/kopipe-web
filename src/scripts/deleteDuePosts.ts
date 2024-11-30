4a405b4b18e1c7a67f3972f532dad9db27c240d9
oukalatte <oukalatte@gmail.com>
2024-11-30 14:16:10 +0800

    update

--- src/scripts/deleteDuePosts.ts
+++ src/scripts/deleteDuePosts.ts
@@ -4,6 +4,7 @@ import axios from "axios";
 export const prisma = new PrismaClient();
 
 const main = async () => {
+  console.log("start checking");
   // 3 hours
   // const dueTime = new Date(Date.now() - 3 * 60 * 60 * 1000);
   const dueTime = new Date(Date.now() - 20 * 1000);
