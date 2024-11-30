b5a8f2b5161d3da54e3e1118adaee0379af27041
oukalatte <oukalatte@gmail.com>
2024-11-30 14:50:45 +0800

    update

--- src/scripts/deleteDuePosts.ts
+++ src/scripts/deleteDuePosts.ts
@@ -1,30 +1,13 @@
 import { PrismaClient } from "@prisma/client";
-import axios from "axios";
 import fs from "fs/promises";
 import path from "path";
 
 export const prisma = new PrismaClient();
 
-const deleteOldFiles = async (folderPath: string, dueTime: Date) => {
-  const files = await fs.readdir(folderPath);
-
-  for (const file of files) {
-    const filePath = path.join(folderPath, file);
-    const stats = await fs.stat(filePath);
-
-    // Check if the file is older than dueTime
-    if (stats.mtime.getTime() < dueTime.getTime()) {
-      await fs.unlink(filePath);
-      console.log(`Deleted file: ${filePath}`);
-    }
-  }
-};
-
 const main = async () => {
-  console.log("start checking");
   // 3 hours
-  // const dueTime = new Date(Date.now() - 3 * 60 * 60 * 1000);
-  const dueTime = new Date(Date.now() - 20 * 1000);
+  const dueTime = new Date(Date.now() - 3 * 60 * 60 * 1000);
+  // const dueTime = new Date(Date.now() - 20 * 1000);
 
   const folderPath = process.env.UPLOAD_DIR ?? "/srv/uploads";
   const files = await fs.readdir(folderPath);
